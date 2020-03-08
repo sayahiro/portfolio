953e043fc672a499c964c2b5b14d65e86d15f56c
satoshi <>
2020-03-08 12:04:47 +0900

    不要なjsを削除

--- templete/page/top/top.js
+++
@@ -1,8 +0,0 @@
-menuIcon = document.querySelector('.menu');
-closeIcon = document.querySelector('.close');
-headerTitle = document.querySelector('.header-title');
-
-menuIcon.addEventListener('click', event => {
-    console.log('捕鯨');
-    headerTitle.style.display = 'none';
-});
